67f3be62cf2ca882c1a5ed0a983b5cbb95bbf0a6
RandelSouza <randelsouza88@gmail.com>
2019-04-24 19:32:16 -0300

    "update"

---
+++ Capítulo 4/Testando_for.java
@@ -0,0 +1,16 @@
+public class Testando_for{
+	public static void main(String[] args){
+		for(int i = 0; i < 10; i++){
+			System.out.println("Teste1 = " + i);
+		}
+
+		System.out.println();
+
+
+		for(int i = 1; i <= 10; i++){
+			System.out.println("Teste2 = " + i);
+
+		}
+
+	}
+}
