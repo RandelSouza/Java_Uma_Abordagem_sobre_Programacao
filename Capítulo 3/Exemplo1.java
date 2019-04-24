361cd2bcf0c1e0d687ad73d1618b842ca1ee7d78
RandelSouza <randelsouza88@gmail.com>
2019-04-24 14:39:49 -0300

    "update"

---
+++ Capítulo 3/Exemplo1.java
@@ -0,0 +1,7 @@
+public class Exemplo1 {
+    public static void escreverBoasVindas(){
+      System.out.println("Olá Pessoal");
+      System.out.println("Bem Vindos ao Curso de Java...");    
+    }
+
+}
