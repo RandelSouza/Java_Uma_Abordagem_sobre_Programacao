a01410a3e172e22d31874c3264e24267962455e9
RandelSouza <randelsouza88@gmail.com>
2019-04-24 14:54:38 -0300

    "update"

--- Capítulo 3/Exemplo2.java
+++ Capítulo 3/Exemplo2.java
@@ -1,15 +1,25 @@
 public class Exemplo2 {
 
-    public static int somar(){
-
+    // Método público que realiza a soma
+    public static int somar(int numero1, int numero2){
+        return numero1 + numero2;
     }
 
-    public static  int subtrair(){
-
+    // Método público que realiza a subtração
+    public static  int subtrair(int numero1, int numero2){
+        return numero1 - numero2;
     }
 
+    // Método principal onde se executa a aplicação
     public static void main( String[] args ){
-      
+        int numero1 = 10;
+        int numero2 = 5;
+
+        // Chamada do método somar e respectiva atribuição do retorno à variável resultadoSoma
+        int resultadoSoma = somar( numero1, numero2 );
+
+        // Chamada do método subtrair e respectiva atribuição do retorno à variável resultadoSubtração
+        int resultadoSubtração = subtrair( numero1, numero2 );
     }
 
 }
