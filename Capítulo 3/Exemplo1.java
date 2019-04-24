0840f47bfcbb8abcc33ea72b7841577fd0ddee03
RandelSouza <randelsouza88@gmail.com>
2019-04-24 14:41:20 -0300

    "update"

--- Capítulo 3/Exemplo1.java
+++ Capítulo 3/Exemplo1.java
@@ -1,7 +1,13 @@
 public class Exemplo1 {
+
     public static void escreverBoasVindas(){
       System.out.println("Olá Pessoal");
-      System.out.println("Bem Vindos ao Curso de Java...");    
+      System.out.println("Bem Vindos ao Curso de Java...");
+    }
+
+    public static void listarChamada(){
+      System.out.println("Estão presentes os cursistas:");
+      System.out.println("João\nMaria\nPedro\nGilberto\nIsabela");
     }
 
 }
