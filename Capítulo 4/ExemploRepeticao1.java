508149b84263882d46cf13e9cdc3c64cc1fc95d3
RandelSouza <randelsouza88@gmail.com>
2019-04-25 19:39:44 -0300

    "update"

--- Capítulo 4/ExemploRepeticao1.java
+++ Capítulo 4/ExemploRepeticao1.java
@@ -1,9 +1,8 @@
 public class ExemploRepeticao1{
 	public static void main( String[] args ){
-	for(){
-	}
-	while(){
-	}
-	do{}while();
-	}
+		// Primeiro declaramos a variável auxiliar
+		int auxiliar = 0;
+		while( auxiliar <= 100000 ){
+		}
+
 }
