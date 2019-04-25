2250d1362df67316c8e1c0d032b248c3e55e9015
RandelSouza <randelsouza88@gmail.com>
2019-04-25 20:46:21 -0300

    "update"

--- Capítulo 4/ExercicioResolvido1.java
+++ Capítulo 4/ExercicioResolvido1.java
@@ -11,7 +11,7 @@ seguinte, a cada vez que for executado o laço while;
 4. Declare a variável media para calcular a média; note que na variável não há
 acento algum (não utilizar também os caracteres especiais como !,@,#...). O
 tipo dela pode ser inteiro, pois na divisão de dois inteiros (soma/contador)
-o resultado é appresentado inteiro.
+o resultado é apresentado inteiro.
 */
 public class ExercicioResolvido1{
     public static void main( String[] args ){
