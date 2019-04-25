fab9007cc95d7953d8bcd63b85fe5889056436c5
RandelSouza <randelsouza88@gmail.com>
2019-04-25 20:47:29 -0300

    "update"

--- Capítulo 4/ExercicioResolvido2.java
+++ Capítulo 4/ExercicioResolvido2.java
@@ -1,5 +1,9 @@
 public class ExercicioResolvido2{
     public static void main( String[] args ){
+        int contador = 1;
+        int soma  = 0;
+        int media = 0;
+
         do{
 
         } while();
