cd9905c1a408538ec86d28e01ce84f79b7ae0008
RandelSouza <randelsouza88@gmail.com>
2019-04-25 20:59:15 -0300

    "update"

--- Capítulo 4/ExercicioResolvido3.java
+++ Capítulo 4/ExercicioResolvido3.java
@@ -1,7 +1,19 @@
 public class ExercicioResolvido3{
     public static void main( String[] args ){
-        for(){
+        int contador = 0;
+        int soma  = 0;
+        int media = 0;
 
+        // Outra maneira de usar a estrutura for
+        for( ; contador < 10; ){
+            contador++;
+            soma = soma + contador;
+            System.out.print("Laço: " + contador);
+            System.out.println("   Soma: " + soma);
         }
+
+        media = soma / contador;
+        System.out.println("Soma dos 10 Primeiros Números: " + soma);
+        System.out.println("Média dos 10 Primeiros Números: " + media);
     }
 }
