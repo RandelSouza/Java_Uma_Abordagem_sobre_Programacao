04a4012a798dedc83efc33afdabca1dcacfcbfde
RandelSouza <randelsouza88@gmail.com>
2019-04-25 20:24:54 -0300

    "update"

--- Capítulo 4/ExercicioResolvido1.java
+++ Capítulo 4/ExercicioResolvido1.java
@@ -1,5 +1,15 @@
+/*
+1. Declare a classe ExercicioResolvido1 como pública, e inicie com o método main()
+2. Declare uma variável contador para contar de 1 a 10;
+3. Declare uma variável soma para acumular o resultado da soma de cada número
+seguinte, a cada vez que for executado o laço while;
+4. Declare a variável media para calcular a média; note que na variável não há
+acento algum (não utilizar também os caracteres especiais como !,@,#...). O
+tipo dela pode ser inteiro, pois na divisão de dois inteiros (soma/contador)
+o resultado é appresentado inteiro.
+*/
 public class ExercicioResolvido1{
-    public static void main( String[] args ){        
+    public static void main( String[] args ){
         while(){
 
         }
