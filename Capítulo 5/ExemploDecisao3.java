956b4b8b33d462bc8fa2a2e3c4816d1dec7af392
RandelSouza <randelsouza88@gmail.com>
2019-04-26 17:24:20 -0300

    "update"

---
+++ Capítulo 5/ExemploDecisao3.java
@@ -0,0 +1,22 @@
+public class ExemploDecisao{
+    public static void main(String[] args) {
+        String nome = "João";
+        int idade = 18; // Supondo que o usuário digite 18.
+
+        if(idade < 10){
+            System.out.println(nome + " é menor e criança.");
+
+        }
+        else if(idade < 18){
+            System.out.println(nome + " é menor e adolescente.");
+
+        }
+        else if(idade < 21){
+            System.out.println(nome + " é maior e jovem adulto.");
+
+        }
+        else{
+            System.out.println(nome + " é maior e adulto.");
+        }
+    }
+}
