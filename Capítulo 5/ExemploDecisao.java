0afe1b1110b382ea3f3f530b4643a1496d833270
RandelSouza <randelsouza88@gmail.com>
2019-04-26 17:36:22 -0300

    "update"

--- Capítulo 5/ExemploDecisao.java
+++ Capítulo 5/ExemploDecisao.java
@@ -1,8 +1,11 @@
 public class ExemploDecisao{
     public static void main(String[] args) {
         String nome = "João";
-        int idade = 18; // Supondo que o usuário digite 18.
-
+         int idade = 18; // Supondo que o usuário digite 18.
+        // int idade = 9; // Exercício 1 a)
+        // int idade = 10; // Exercício 1 b)
+        // int idade = 20; // Exercício 1 c)
+        // int idade = 21; // Exercício 1 d)
         if(idade < 10){
             System.out.println(nome + " é menor e criança.");
 
