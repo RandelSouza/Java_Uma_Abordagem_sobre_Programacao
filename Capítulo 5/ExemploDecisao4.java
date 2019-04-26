e85f7163d20d000862a9606fe7146d2829fe8d15
RandelSouza <randelsouza88@gmail.com>
2019-04-26 17:28:43 -0300

    "update"

---
+++ Capítulo 5/ExemploDecisao4.java
@@ -0,0 +1,20 @@
+public class ExemploDecisao3{
+    public static void main(String[] args) {
+        int num = 10;
+
+        switch(num) {
+            case 5:
+                System.out.println("É cinco.");
+                break;
+
+            case 10:
+                System.out.println("É dez.");
+                System.out.println("Pode haver mais de um comando em casa case.");
+                break;
+
+            case 20:
+                System.out.println("É vinte.");
+                break;
+        }
+    }
+}
