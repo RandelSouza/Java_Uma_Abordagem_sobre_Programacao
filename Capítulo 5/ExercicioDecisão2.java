f63f31340a44eee4c4a8b163f811e6b3708ed0bf
RandelSouza <randelsouza88@gmail.com>
2019-04-26 17:37:31 -0300

    "update"

---
+++ Capítulo 5/ExercicioDecisão2.java
@@ -0,0 +1,22 @@
+public class ExemploDecisao4{
+    public static void main(String[] args) {
+        char letra = 'x';
+
+        switch(letra) {
+            case 'c':
+                System.out.println("letra c.");
+                break;
+
+            case 'p':
+                System.out.println("letra p.");
+                break;
+
+            case 'u':
+                System.out.println("letra u.");
+                break;
+            default:
+                System.out.println("Não foram encontradas 'c', 'p' e enm 'u'.");
+                break;
+        }
+    }
+}
