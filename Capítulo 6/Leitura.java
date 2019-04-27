de688f99fec115678f3ec3446c215277b2244681
RandelSouza <randelsouza88@gmail.com>
2019-04-27 11:48:17 -0300

    "update"

--- Capítulo 6/Leitura.java
+++ Capítulo 6/Leitura.java
@@ -5,22 +5,26 @@ public class Leitura{
     public static void main(String[] args) {
         Scanner ler = new Scanner(System.in);
 
-        System.out.println("Digite seu nome: ");
+        System.out.print("Digite seu nome: ");
         String nome = ler.next();
 
-        System.out.println("Digite um número inteiro (tipo byte): ");
+        System.out.print("Digite um número inteiro (tipo byte): ");
         byte num1 = ler.nextByte();
 
-        System.out.println("Digite um número inteiro (tipo short): ");
+        System.out.print("Digite um número inteiro (tipo short): ");
         short num2 = ler.nextShort();
 
-        System.out.println("Digite um número inteiro (tipo int): ");
+        System.out.print("Digite um número inteiro (tipo int): ");
         int num3 = ler.nextInt();
 
-        System.out.println("Digite um número inteiro (tipo long): ");
+        System.out.print("Digite um número inteiro (tipo long): ");
         long num4 = ler.nextInt();
 
-        System.out.println(nome + "\n" + num1 + "\n" + num2 + "\n" + num3 + "\n" + num4 );
+        System.out.println(nome + "\n" + num1 + "\n" + num2 + "\n" + num3 + "\n" + num4
+
+
+
+         );
     }
 
 }
