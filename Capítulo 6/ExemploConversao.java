06c5d3dbbfdcbe06b1a2d3db05ff04c0fd557b6c
RandelSouza <randelsouza88@gmail.com>
2019-04-27 12:24:40 -0300

    "update"

--- Capítulo 6/ExemploConversao.java
+++ Capítulo 6/ExemploConversao.java
@@ -7,13 +7,13 @@ public class ExemploConversao{
 
         Byte num1 = Byte.parseByte(numero);
         short num2  = Short.parseShort(numero);
-        Integer num3  = Integer.parseInteger(numero);
+        Integer num3  = Integer.parseInt(numero);
         Long num4  = Long.parseLong(numero);
         Float num5  = Float.parseFloat(numero);
         Double num6  = Double.parseDouble(numero);
 
         JOptionPane.showMessageDialog(null, "Os números convertidos são: " + num1 +
-        "\n" + num2 + "\n" "\n" + num3 + "\n" + num4 + "\n" + num5 + "\n" + num6);
+        "\n" + num2 + "\n" + num3 + "\n" + num4 + "\n" + num5 + "\n" + num6);
     }
 
 }
