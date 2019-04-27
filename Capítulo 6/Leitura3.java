f9686c34671a9415f34a12389e4831eb0659fd02
RandelSouza <randelsouza88@gmail.com>
2019-04-27 12:00:21 -0300

    "update"

---
+++ Capítulo 6/Leitura3.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Leitura3{
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = ler.next();
+
+        System.out.print("Digite seu endereço: ");
+        String endereco = ler.nextLine();
+
+        System.out.print("Digite seu recado: ");
+        String recado = ler.nextLine();
+
+        System.out.println("Resultado: " + "\n"+ nome + "\n" + endereco + "\n" + recado);
+    }
+
+}
