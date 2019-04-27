0a85aead886d0fab9b186bff716c9d0c468f5bbb
RandelSouza <randelsouza88@gmail.com>
2019-04-27 12:08:48 -0300

    "update"

---
+++ Capítulo 6/Leitura4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Leitura4{
+
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+
+        System.out.print("Digite seu nome: ");
+        String nome = ler.nextLine();
+
+        System.out.print("Digite seu endereço: ");
+        String endereco = ler.nextLine();
+
+        System.out.print("Digite seu recado: ");
+        String recado = ler.nextLine();
+
+        System.out.println("Resultado: " + "\n"+ nome + "\n" + endereco + "\n" + recado);
+    }
+
+}
