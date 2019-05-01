13620b6fb028e7d8b9628d71d432e36eafca05a4
RandelSouza <randelsouza88@gmail.com>
2019-05-01 19:12:17 -0300

    "update Capítulo 6"

--- Capítulo 6/Exercicio4.java
+++ Capítulo 6/Exercicio4.java
@@ -1,6 +1,16 @@
 import java.util.Scanner;
 
 public class Exercicio4{
+
+    public static void menu(){
+        
+        System.out.println("[ 1 ] Inserir novo paciente");
+        System.out.println("[ 2 ] Inserir novo médico");
+        System.out.println("[ 3 ] Marcar consulta");
+        System.out.println("[ 4 ] Desmarcar consulta");
+        System.out.println("[ 0 ] Sair");
+    }
+
     public static void main(String[] args) {
         String nome  = "";
         int numero = 0;
