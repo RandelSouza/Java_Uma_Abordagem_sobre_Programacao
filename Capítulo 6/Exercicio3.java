90afa41a5b43ae64ad6b7a65e3036b58471725ab
RandelSouza <randelsouza88@gmail.com>
2019-05-01 19:05:04 -0300

    "update Capítulo 6"

--- Capítulo 6/Exercicio3.java
+++ Capítulo 6/Exercicio3.java
@@ -2,6 +2,23 @@ import java.util.Scanner;
 
 public class Exercicio2{
     public static void main(String[] args) {
+        String nome, sobrenome, logradouro = "";
+        int numero = 0;
+        Scanner entrada = new Scanner(System.in);
 
+        System.out.print("Digite seu primeiro nome: ");
+        nome = entrada.nextLine();
+
+        System.out.print("Digite seu sobrenome: ");
+        sobrenome = entrada.nextLine();
+
+        System.out.print("Digite o seu endereco na forma de Logradouro: ");
+        logradouro = entrada.nextLine();
+
+        System.out.print("Digite o seu número: ");
+        numero = Integer.parseInt(entrada.nextLine());
+
+        System.out.print("Nome: " + nome +", "+"Sobrenome: " + sobrenome+", "
+                        +"Logradouro: " + logradouro+", "+"Número: " + numero);
     }
 }
