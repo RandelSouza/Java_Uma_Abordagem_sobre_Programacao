95e895e4e93abb88d8ba68404c056bec9076e5c8
RandelSouza <randelsouza88@gmail.com>
2019-05-01 19:08:58 -0300

    "update Capítulo 6"

---
+++ Capítulo 6/Exercicio4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Exercicio4{
+    public static void main(String[] args) {
+        String nome  = "";
+        int numero = 0;
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.print("Digite seu primeiro nome: ");
+        nome = entrada.nextLine();
+
+    }
+}
