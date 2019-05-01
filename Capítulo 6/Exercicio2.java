7816b394b6db368a352211b16293e1eff888f3ac
RandelSouza <randelsouza88@gmail.com>
2019-05-01 18:51:21 -0300

    "update Capítulo 6"

---
+++ Capítulo 6/Exercicio2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Exercicio2{
+    public static void main(String[] args) {
+        float cotacao = 0f;
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Qual a cotação para o dia de hoje?: ");
+        cotacao = Float.parseFloat(entrada.nextLine()) * 3.92;
+        System.out.println("O valor da cotação em reais é: " + cotação);
+    }
+}
