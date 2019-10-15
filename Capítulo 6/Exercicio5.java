6f97d740d46f9f32a8906b64c2e21e0e174a56ba
RandelSouza <randelsouza88@gmail.com>
2019-10-15 11:34:59 -0300

    "updating"

--- Capítulo 6/Exercicio5.java
+++ Capítulo 6/Exercicio5.java
@@ -2,6 +2,12 @@ import javax.swing.JOptionPane;
 
 public class Exercicio5{
     public static void main(String[] args) {
+        int numero = Integer.parseInt(JOptionPane.showInputDialog("Digite um número inteiro, e descubra se é par ou impar: "));
 
+        if(numero % 2 == 0){
+            JOptionPane.showMessageDialog(null, "O número " + String.valueOf(numero) +" é Par!");
+        }else{
+            JOptionPane.showMessageDialog(null, "O número " + String.valueOf(numero) +" é Impar!");
+        }
     }
 }
