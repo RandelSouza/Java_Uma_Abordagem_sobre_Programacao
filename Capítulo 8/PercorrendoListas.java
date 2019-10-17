5274168622dea713f3dc8e9c9d8c3432371475f8
RandelSouza <randelsouza88@gmail.com>
2019-10-17 11:47:38 -0300

    "capítulo 8 percorrendo listas"

---
+++ Capítulo 8/PercorrendoListas.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
+public class PercorrendoListas{
+    public static void main(String[] args) {
+        List listaMeses = new ArrayList();
+
+        for(int i = 0; i < 12; i++){
+            listaMeses.add(JOptionPane.showInputDialog("Digite o " + (i+1) + "° + Mês: " ));
+        }
+
+        for(int i = 0; i < listaMeses.size(); i++){
+            System.out.println("Mês: " + listaMeses.get(i) );
+        }
+    }
+}
