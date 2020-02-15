f93e13e9b78f86237656e3d029098717c142c406
RandelSouza <randelsouza88@gmail.com>
2020-02-15 10:02:15 -0300

    add códigos e exercícios

---
+++ Capítulo 10/TesteAtleta.java
@@ -0,0 +1,16 @@
+public class TesteAtleta{
+    public static void main(String[] args) {
+            Atleta atleta = new Atleta();
+
+            atleta.codigo = 10;
+            atleta.nome = "Joaquim";
+            atleta.esporte = "Atletismo";
+            atleta.valorPatrocinio = 3980.55;
+
+            System.out.println("Objeto atleta criado a partir da classe Atleta.");
+            System.out.println("Codigo: " + atleta.codigo);
+            System.out.println("Nome: " + atleta.nome);
+            System.out.println("Esporte: " + atleta.esporte);
+            System.out.println("Valor de Patrocinio: R$ " + atleta.valorPatrocinio);
+    }
+}
